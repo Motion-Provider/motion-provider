ba80aee1c8a3fdf798ac59408b538f4bb8a36062
Masculinn <hello@burakdev.com>
2025-09-05 01:20:24 +0200

    fix: - huge code quality improvements - fixed critical eslint issues - speed index increased by 12% - now custom error handler works seemlesly for mistyped animation modes

--- src/motion/motion-container.tsx
+++ src/motion/motion-container.tsx
@@ -3,14 +3,14 @@ import {
   MOTION_CONTAINER_ANIMATION_DEFAULT,
   MOTION_CONTAINER_CONTROLLER_DEFAULT,
 } from "./lib/defaults.lib";
-import animations from "./lib/animate.lib";
 import logError from "./utils/getErrorLogs";
-import { MotionContainerProps } from "./types";
-import transitions from "./lib/transitions.lib";
+import { AnimationKeys, AnimationModule, MotionContainerProps } from "./types";
 import { motion, useInView } from "motion/react";
-import React, { FC, useId, useMemo, useRef } from "react";
-import { AnimationObjProps, TransitionConfig } from "./types";
+import React, { FC, useMemo, useRef } from "react";
+import { TransitionConfig } from "./types";
 import { useAnimationMixer } from "./hooks/use-animation-mixer";
+import transitions from "./constants/transitions";
+import animations from "./constants/animations";
 
 const MotionContainer: FC<MotionContainerProps> = ({
   animation = { ...MOTION_CONTAINER_ANIMATION_DEFAULT },
@@ -32,64 +32,58 @@ const MotionContainer: FC<MotionContainerProps> = ({
     trigger,
     reverse,
   } = controller;
+
   const ref = useRef<HTMLElement | null>(null);
   const isInView = useInView(ref, configView);
-  const id = useId();
 
-  if (typeof animation === "undefined") {
-    logError({
-      error: "Animation is undefined, returning null.",
-      mod: "error",
-      src: "MotionContainer",
-    });
-    return null;
-  }
-
-  const animationsToMix = useMemo(() => {
-    return Array.isArray(mode)
-      ? mode.map((key) => animations[key] || { initial: {}, animate: {} })
-      : [animations[mode] || { initial: {}, animate: {} }];
-  }, [mode, trigger]);
-
+  const mix = useMemo(() => getAnimationsToMix(mode), [mode]);
   const { initial, animate } = useAnimationMixer({
-    animations: animationsToMix as AnimationObjProps[],
+    animations: mix,
     reverse,
   });
 
   const transitionConfig: TransitionConfig = useMemo(() => {
-    const defaultTransition = transitions[transition || "default"];
+    const selectedTransition = transitions[transition || "default"];
+
     if (isAnimationStopped) {
       return {
-        ...defaultTransition,
-        duration: duration || defaultTransition.duration,
+        ...selectedTransition,
+        duration: duration || selectedTransition.duration,
         delay: 0,
       };
     }
+
     return {
-      ...defaultTransition,
-      duration: duration || defaultTransition.duration,
-      delay: delay || defaultTransition.delay,
+      ...selectedTransition,
+      duration: duration || selectedTransition.duration,
+      delay: delay || 0,
     };
-  }, [isAnimationStopped, trigger]);
+  }, [delay, duration, isAnimationStopped, transition]);
 
   const animationState = useMemo(() => {
-    if (isAnimationStopped) {
+    if (isAnimationStopped)
       return { ...animations["opacity"].animate, ...animate };
-    }
 
-    if (typeof trigger !== "undefined") {
-      return trigger ? animate : initial;
-    }
+    if (typeof trigger !== "undefined") return trigger ? animate : initial;
 
     return isInView ? animate : initial;
   }, [isAnimationStopped, isInView, initial, animate, trigger]);
 
   const initialState = useMemo(() => {
-    if (isAnimationStopped) {
+    if (isAnimationStopped)
       return { ...animations["opacity"].initial, ...initial };
-    }
+
     return initial;
   }, [isAnimationStopped, initial]);
+
+  if (!animation || typeof animation === "undefined") {
+    logError({
+      msg: "Animation cannot be undefined or null, returning null.",
+      mod: "error",
+      src: "MotionContainer",
+    });
+    return null;
+  }
 
   const MotionElement = motion[
     elementType as keyof typeof motion
@@ -97,18 +91,18 @@ const MotionContainer: FC<MotionContainerProps> = ({
 
   if (!MotionElement) {
     logError({
-      error: `Invalid motion elementType: '${elementType}'`,
+      msg: `Invalid motion elementType: '${elementType}'`,
       mod: "error",
       src: "MotionContainer",
     });
     return null;
   }
+
   return React.createElement(
     MotionElement,
     {
       className,
       ref,
-      key: id,
       initial: initialState,
       animate: animationState,
       transition: transitionConfig,
@@ -116,5 +110,74 @@ const MotionContainer: FC<MotionContainerProps> = ({
     children
   );
 };
+
+const blankAnimationObj = {
+  initial: {},
+  animate: {},
+};
+
+function getAnimationsToMix(
+  mode: AnimationKeys[] | AnimationKeys
+): AnimationModule[] | AnimationModule {
+  if (
+    !mode ||
+    typeof mode === "undefined" ||
+    (Array.isArray(mode) && mode.length <= 0)
+  ) {
+    logError({
+      mod: "error",
+      msg: "Mode prop cannot be either 'null', 'undefined' or an empty array!",
+      src: "CoreMotion",
+    });
+    return animations["default"];
+  }
+
+  const allModes = Object.keys(animations);
+
+  if (Array.isArray(mode)) {
+    const checkModeIsValid = mode.every((key) => allModes.includes(key));
+
+    if (!checkModeIsValid) {
+      logError({
+        mod: "error",
+        msg: `One or more of 'mode' member(s) are not matching with the pre-defined animation list. Check the list:
+${allModes.map((k) => `${k}\n`).join("- ")}
+`,
+        src: "CoreMotion",
+      });
+
+      return animations["default"];
+    }
+
+    const extractedModes = mode.map((key) => animations[key]);
+    return extractedModes;
+  } else {
+    if (typeof mode === "string") {
+      const checkModeIsValid = allModes.includes(mode);
+
+      if (!checkModeIsValid) {
+        logError({
+          mod: "error",
+          msg: `The 'mode' you select is not matching with the pre-defined animation list.  
+Check the list:
+${allModes.map((k) => `${k}\n`).join("- ")}
+`,
+          src: "CoreMotion",
+        });
+        return blankAnimationObj;
+      }
+
+      return animations[mode];
+    }
+
+    logError({
+      mod: "error",
+      msg: `Mode prop must be either 'string' or 'string[]'!`,
+      src: "CoreMotion",
+    });
+
+    return animations["default"];
+  }
+}
 
 export default MotionContainer;
