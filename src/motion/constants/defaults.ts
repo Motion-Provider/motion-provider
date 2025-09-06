b4db0349d05818a931482010b1c3983a29090cb5
Masculinn <hello@burakdev.com>
2025-09-06 12:49:46 +0200

    feat: Mitigated the overhead on base 'MotionContainer' MP component by migrating from scattered legacy lib file to all-in-one object orianted codebase.

---
+++ src/motion/constants/defaults.ts
@@ -0,0 +1,68 @@
+﻿import { createElement } from "react";
+import { MotionChainProps, MotionDefaultsProps } from "../types";
+
+export default {
+  MotionContainer: {
+    controller: {
+      configView: { once: true, amount: 0.5 },
+      isAnimationStopped: false,
+      trigger: undefined,
+      reverse: false,
+    },
+    animation: {
+      mode: "opacity",
+      transition: "smooth",
+      delay: 0,
+      duration: 0.5,
+    },
+    elementType: "div",
+    children: undefined,
+    className: undefined,
+  },
+  MotionChain: {
+    controller: undefined as MotionChainProps["controller"],
+    elementType: "div",
+    config: {
+      duration: 0.5,
+      customLogic: undefined,
+      delayLogic: "linear",
+      delayByElement: undefined,
+    },
+  },
+  MotionImage: {
+    config: {
+      duration: 3,
+      pieces: 64,
+      delayLogic: "sinusoidal",
+      customLogic: undefined,
+      delayByElement: undefined,
+      img: undefined,
+      fn: undefined,
+    },
+    fallback: createElement("div", {
+      className: "w-full h-full absolute bg-stone-950 animate-pulse",
+    }),
+  },
+  MotionLink: {
+    timer: 2000,
+    href: "/",
+  },
+  MotionText: {
+    config: {
+      duration: 0.5,
+      mode: "chars",
+      space: 2,
+      delayLogic: "linear",
+    },
+  },
+  MotionMovie: {
+    config: {
+      pieces: 64,
+      images: [],
+      animationDuration: 2,
+    },
+    fallback: createElement("div", {
+      className: "w-full h-full absolute bg-stone-950 animate-pulse",
+    }),
+  },
+} as const satisfies MotionDefaultsProps;
