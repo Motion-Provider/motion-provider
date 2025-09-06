935970272764b103f94b986e0cd51c30d740b5cd
Masculinn <hello@burakdev.com>
2025-09-07 00:01:14 +0200

    feat: started to add jsdocs for the types

--- src/motion/types/index.ts
+++ src/motion/types/index.ts
@@ -1,28 +1,44 @@
-/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**/
-// Welcome to the Motion Provider Interfaces
-// Author: Burak Bilen
-// Warning: Any changes in this file may cause fatal type errors in the application. Be aware.
-/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**/
-
-// Imports
-
 import { EasingDefinition, UseInViewOptions } from "motion/react";
 import { HTMLAttributes } from "react";
 
-/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**/
-
-// Cores
-
 export interface MotionControllerProps {
-  configView?: UseInViewOptions;
+  /**
+   * @description
+   * Allows you to pass options to the useInView hook,
+   * which is used to control the animation with viewport.
+   *
+   * @see https://motion.dev/docs/react-use-in-view
+   */
+  configView?: Omit<UseInViewOptions, "root">;
+  /**
+   * @description
+   * An immediate animation caller — if true the relevant
+   * MP component won't listen any other prop to start animate
+   */
   trigger?: boolean;
   isAnimationStopped?: boolean;
   reverse?: boolean;
 }
 
+/**
+ * The MotionAnimationProps interface defines properties that can be
+ * passed to the MotionContainer component.
+ *
+ * @typedef {Object} MotionAnimationProps
+ * @property {AnimationKeys | AnimationKeys[]} mode - animation mode(s)
+ * @property {TransitionKeys} transition - animation transition type
+ * @property {number | undefined} [delay] - animation delay
+ * @property {number} [duration] - animation duration
+ */
 export interface MotionAnimationProps {
+  /**
+   * @description
+   */
   mode: AnimationKeys | AnimationKeys[];
   transition: TransitionKeys;
+  /**
+   * @property {number=} delay of the animation
+   */
   delay?: number;
   duration?: number;
 }
@@ -68,6 +84,40 @@ export interface MotionMovieAnimationsProps
 
 type GeneralHTMLAttributes = Omit<HTMLAttributes<HTMLElement>, "children">;
 
+/**
+ * @description
+ * The MotionContainer component is a powerful wrapper that
+ * provides out-of-the-box animation support for any element
+ * you picked.
+ *
+ * @example
+ *
+ *  <MotionContainer
+ *    elementType="div"
+ *    animation={{
+ *      mode: ["fadeIn", "filterBlurIn"],
+ *      transition: "smooth",
+ *      duration: 1,
+ *    }}
+ *    className="your-css-goes-here"
+ *  >
+ *    <MyChildComponent />
+ *  </MotionContainer>
+ *
+ * @param {MotionContainerProps} props The component props.
+ * @typedef {Object} MotionContainerProps
+ * @param {AnimationKeys | AnimationKeys[]} animation.mode - animation mode(s)
+ * @param {TransitionKeys} animation.transition - animation transition type
+ * @param {number | undefined} [animation.delay] - animation delay
+ * @param {number} [animation.duration] - animation duration
+ * @param {Partial<ControllerProps>} [controller] - animation controller
+ * @param {React.ReactNode} children - children node
+ * @param {React.ElementType} [elementType] - element type
+ * @param {string} [className] - className
+ * @param {React.HTMLAttributes<HTMLElement>} [props] - additional props
+ *
+ * @returns {React.ReactElement} The rendered component.
+ */
 export interface MotionContainerProps extends GeneralHTMLAttributes {
   animation: MotionAnimationProps;
   elementType: React.ElementType;
@@ -147,15 +197,14 @@ export type GetSplittedTextOutputProps = string[];
 
 /*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**/
 
-// Libs
+// Constants
 
 export interface AnimationLibraryProps {
-  [key: string]: {
+  [key: string | AnimationKeys]: {
     initial: AnimationObjProps;
     animate: AnimationObjProps;
   };
 }
-
 export type AnimationModule = {
   initial: AnimationObjProps;
   animate: AnimationObjProps;
@@ -164,6 +213,9 @@ export interface TransitionConfig {
   duration?: number;
   ease?: EasingDefinition | number[];
   delay?: number;
+}
+export interface Transitions {
+  [key: string | TransitionKeys]: TransitionConfig;
 }
 /*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**/
 
@@ -314,11 +366,16 @@ export type AnimationKeys =
   | "rotateBounce"
   | "elasticBounce"
   | "bounceInOut"
-  | "burakHeartbeat"
-  | "burakRubberBand"
-  | "burakWobble"
-  | "burakPulse"
-  | "skewX"
+  | "heartbeat"
+  | "heartbeatRubber"
+  | "wobble"
+  | "pulse"
+  | "skewX45"
+  | "skewX90"
+  | "skewX180"
+  | "skewY45"
+  | "skewY90"
+  | "skewY180"
   | "textShimmer"
   | "swingHorizontal"
   | "flash"
@@ -367,12 +424,7 @@ export type AnimationKeys =
   | "jellyTwist"
   | "depthPush"
   | "colorShift"
-  | "orbitRotation"
-  | "moveToRightBottom"
-  | "moveToRightTop"
-  | "moveToLeftBottom"
-  | "moveToTopCenter"
-  | "moveToLeftTop";
+  | "orbitRotation";
 
 export type TransitionKeys =
   | "none"
