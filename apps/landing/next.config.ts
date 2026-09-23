71cea6c3e3234e6a59cae3fccd6a08bb9f447ed1
Masculinn <hello@burakdev.com>
2026-09-24 00:05:45 +0200

    chore: configure turbo with the apps

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  reactStrictMode: true,
+	reactCompiler: true,
+	reactStrictMode: true,
 };
 
 export default nextConfig;
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,24 +3,24 @@
 import type { ReactNode } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+	children: ReactNode;
+	className?: string;
+	appName: string;
 }
 
 export const Button = ({
-  children,
-  className,
-  appName,
-  ...props
+	children,
+	className,
+	appName,
+	...props
 }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-      {...props}
-    >
-      {children}
-    </button>
-  );
+	return (
+		<button
+			className={className}
+			onClick={() => alert(`Hello from your ${appName} app!`)}
+			{...props}
+		>
+			{children}
+		</button>
+	);
 };
